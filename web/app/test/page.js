b2af740eac273c3ed5ac11821d071b38a98a9ef6
Rodrigo Pombo <pombopombopombo@gmail.com>
2023-01-05 23:15:00 +0000

    Load grammars once

--- web/app/test/page.js
+++ web/app/test/page.js
@@ -3,6 +3,17 @@ import { Code } from "./code";
 export default function Page() {
   return (
     <div>
+      <Code lang="md" theme="slack-dark">
+        {`# hello
+
+~~~js
+console.log(1)
+~~~
+
+~~~py
+print 2
+~~~`}
+      </Code>
       <Code lang="py" theme="slack-dark">
         print 2
       </Code>
