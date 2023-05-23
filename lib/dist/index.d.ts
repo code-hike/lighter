d7135e737f3d1819716fefedd10d559a2060540e
Ian Ownbey <ian@heypierre.app>
2023-05-23 15:16:54 -0400

    0.6.6

--- lib/dist/index.d.ts
+++ lib/dist/index.d.ts
@@ -24,7 +24,7 @@ type FinalTheme = {
         [key: string]: string;
     };
 };
-declare const THEME_NAMES: readonly ["dark-plus", "dracula-soft", "dracula", "github-dark", "github-dark-dimmed", "github-light", "light-plus", "material-darker", "material-default", "material-lighter", "material-ocean", "material-palenight", "min-dark", "min-light", "monokai", "nord", "one-dark-pro", "poimandres", "slack-dark", "slack-ochin", "solarized-dark", "solarized-light"];
+declare const THEME_NAMES: readonly ["dark-plus", "dracula-soft", "dracula", "github-dark", "github-dark-dimmed", "github-light", "light-plus", "material-darker", "material-default", "material-lighter", "material-ocean", "material-palenight", "min-dark", "min-light", "monokai", "nord", "one-dark-pro", "poimandres", "slack-dark", "slack-ochin", "solarized-dark", "solarized-light", "base16"];
 type NamesTuple$1 = typeof THEME_NAMES;
 type StringTheme = NamesTuple$1[number];
 type Theme = StringTheme | RawTheme;
