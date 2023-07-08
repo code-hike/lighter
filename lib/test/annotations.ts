5058c36952c4332121bc5e6909ba339cd1d858c7
Rodrigo Pombo <pombopombopombo@gmail.com>
2023-07-08 15:49:44 +0000

    Add annotation tests

--- lib/test/annotations.ts
+++ lib/test/annotations.ts
@@ -28,4 +28,32 @@ const y = 2;`.trim();
     });
     expect(hResult).toMatchSnapshot();
   });
+
+  test("extract annottations from txt", async () => {
+    const code = `
+const x = 1;
+# foo[3:5]
+const y = 2;`.trim();
+
+    const result = await extractAnnotations(code, "txt", ["foo"]);
+    expect(result).toMatchSnapshot();
+    const hResult = await highlight(result.code, "txt", "dark-plus", {
+      annotations: result.annotations,
+    });
+    expect(hResult).toMatchSnapshot();
+  });
+
+  test("extract annottations from mdx", async () => {
+    const code = `
+{/* foo[3:5] */}
+# Hi there
+{/* bar */}`.trim();
+
+    const result = await extractAnnotations(code, "mdx", ["foo"]);
+    expect(result).toMatchSnapshot();
+    const hResult = await highlight(result.code, "mdx", "dark-plus", {
+      annotations: result.annotations,
+    });
+    expect(hResult).toMatchSnapshot();
+  });
 }
